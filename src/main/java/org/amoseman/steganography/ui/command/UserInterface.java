590532e4925b8f46efefff7714ed767139ea98a0
a-moseman <drewmoseman@gmail.com>
2024-10-11 12:52:52 -0400

    Refactor magic strings into constants

--- src/main/java/org/amoseman/steganography/ui/command/UserInterface.java
+++ src/main/java/org/amoseman/steganography/ui/command/UserInterface.java
@@ -4,6 +4,15 @@ package org.amoseman.steganography.ui.command;
  * Provides a user interface for the program.
  */
 public class UserInterface {
+    public static final String ARGUMENT_PREFIX = "--";
+    public static final String ENCODE_COMMAND = "encode";
+    public static final String DECODE_COMMAND = "decode";
+    public static final String ANALYZE_COMMAND = "analyze";
+    public static final String SOURCE_ARGUMENT = "source";
+    public static final String TARGET_ARGUMENT = "target";
+    public static final String COMPRESS_ARGUMENT = "compress";
+    public static final String PASSWORD_ARGUMENT = "password";
+
     private final ArgParser parser;
 
     /**
@@ -11,14 +20,14 @@ public class UserInterface {
      */
     public UserInterface() {
         this.parser = new ArgParser.Builder()
-                .setPrefix("--")
-                .addCommand("encode")
-                .addCommand("decode")
-                .addCommand("analyze")
-                .addBoolean("compress")
-                .addVariable("password", false)
-                .addList("source", false)
-                .addList("target", true)
+                .setPrefix(ARGUMENT_PREFIX)
+                .addCommand(ENCODE_COMMAND)
+                .addCommand(DECODE_COMMAND)
+                .addCommand(ANALYZE_COMMAND)
+                .addList(SOURCE_ARGUMENT, false)
+                .addList(TARGET_ARGUMENT, true)
+                .addBoolean(COMPRESS_ARGUMENT)
+                .addVariable(PASSWORD_ARGUMENT, false)
                 .build();
     }
 
@@ -39,42 +48,42 @@ public class UserInterface {
      */
     private void assertValid(Parameters parameters) {
         switch (parameters.command()) {
-            case "analyze" -> {
-                if (parameters.has("source")) {
+            case ANALYZE_COMMAND -> {
+                if (parameters.has(SOURCE_ARGUMENT)) {
                     throw new IllegalArgumentException("The --source argument is not used for analyze.");
                 }
-                if (parameters.has("compress")) {
+                if (parameters.has(COMPRESS_ARGUMENT)) {
                     throw new IllegalArgumentException("The --compress argument is not used for analyze.");
                 }
-                if (parameters.has("password")) {
+                if (parameters.has(PASSWORD_ARGUMENT)) {
                     throw new IllegalArgumentException("The --password argument is not used for analyze.");
                 }
             }
-            case "encode" -> {
-                if (!parameters.has("source")) {
+            case ENCODE_COMMAND -> {
+                if (!parameters.has(SOURCE_ARGUMENT)) {
                     throw new IllegalArgumentException("The --source argument is required when encoding.");
                 }
-                if (parameters.get("source").length != 1) {
+                if (parameters.get(SOURCE_ARGUMENT).length != 1) {
                     throw new IllegalArgumentException("One file path is required for the --source argument when encoding.");
                 }
-                if (parameters.get("target").length == 0) {
+                if (parameters.get(TARGET_ARGUMENT).length == 0) {
                     throw new IllegalArgumentException("At least one file path is required for the --target argument when encoding.");
                 }
-                if (!parameters.has("source")) {
+                if (!parameters.has(SOURCE_ARGUMENT)) {
                     throw new IllegalArgumentException("The --source argument is required for encoding and decoding.");
                 }
             }
-            case "decode" -> {
-                if (!parameters.has("source")) {
+            case DECODE_COMMAND -> {
+                if (!parameters.has(SOURCE_ARGUMENT)) {
                     throw new IllegalArgumentException("The --source argument is required when decoding.");
                 }
-                if (parameters.get("source").length == 0) {
+                if (parameters.get(SOURCE_ARGUMENT).length == 0) {
                     throw new IllegalArgumentException("At least one file path is required for the --source argument when decoding.");
                 }
-                if (parameters.get("target").length != 1) {
+                if (parameters.get(TARGET_ARGUMENT).length != 1) {
                     throw new IllegalArgumentException("One file path is required for the --target argument when decoding.");
                 }
-                if (!parameters.has("source")) {
+                if (!parameters.has(SOURCE_ARGUMENT)) {
                     throw new IllegalArgumentException("The --source argument is required for encoding and decoding.");
                 }
             }
