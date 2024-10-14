34b42c36e5e1b534a6033a54467e7529e6b92639
a-moseman <drewmoseman@gmail.com>
2024-10-14 12:05:58 -0400

    Add descriptive comments to unit tests for UI

--- src/test/java/org/amoseman/steganography/ui/command/ArgParserTest.java
+++ src/test/java/org/amoseman/steganography/ui/command/ArgParserTest.java
@@ -16,17 +16,31 @@ class ArgParserTest {
                 .addVariable("var", true)
                 .build();
 
+        // valid arguments
         assertDoesNotThrow(() -> parser.parse("run", "--bool", "--arr", "a", "b", "--list", "c", "d", "e", "--var", "f"));
         assertDoesNotThrow(() -> parser.parse("run", "--bool", "--arr", "a", "b", "--list", "--var", "f"));
 
+        // invalid arguments
+
+        // missing command
         assertThrows(IllegalArgumentException.class, () -> parser.parse());
+        // missing arguments
         assertThrows(IllegalArgumentException.class, () -> parser.parse("run"));
+        // --arr is missing values
         assertThrows(IllegalArgumentException.class, () -> parser.parse("run", "--arr"));
+        // --list is not a valid value for --a
         assertThrows(IllegalArgumentException.class, () -> parser.parse("run", "--arr", "--list", "--var"));
+        // --var is missing value
+        assertThrows(IllegalArgumentException.class, () -> parser.parse("run", "--var"));
+        // d is orphan value
         assertThrows(IllegalArgumentException.class, () -> parser.parse("run", "--arr", "a", "b", "--list", "--var", "c", "d"));
+        // blah is an invalid command
         assertThrows(IllegalArgumentException.class, () -> parser.parse("blah"));
+        // --blah is an invalid argument
         assertThrows(IllegalArgumentException.class, () -> parser.parse("run", "--blah"));
+        // blah is not an argument
         assertThrows(IllegalArgumentException.class, () -> parser.parse("run", "blah"));
+        // duplicate argument --var
         assertThrows(IllegalArgumentException.class, () -> parser.parse("run", "--bool", "--arr", "a", "b", "--list", "c", "d", "e", "--var", "f", "--var", "f"));
     }
 
--- src/test/java/org/amoseman/steganography/ui/command/UserInterfaceTest.java
+++ src/test/java/org/amoseman/steganography/ui/command/UserInterfaceTest.java
@@ -10,25 +10,37 @@ class UserInterfaceTest {
     void run() {
         UserInterface ui = new UserInterface();
 
+        // missing command
         assertThrows(IllegalArgumentException.class, () -> ui.parse());
+        // missing --source and/or --target
         assertThrows(IllegalArgumentException.class, () -> ui.parse("encode"));
         assertThrows(IllegalArgumentException.class, () -> ui.parse("encode", "--source"));
         assertThrows(IllegalArgumentException.class, () -> ui.parse("encode", "--target"));
+        // --source and --target lack values
         assertThrows(IllegalArgumentException.class, () -> ui.parse("encode", "--source", "--target"));
+        // --source needs one value, --target needs many
         assertThrows(IllegalArgumentException.class, () -> ui.parse("encode", "--source", "a", "b", "--target", "a"));
 
+        // missing command
         assertThrows(IllegalArgumentException.class, () -> ui.parse("decode"));
+        // missing --source and/or --target
         assertThrows(IllegalArgumentException.class, () -> ui.parse("decode", "--source"));
         assertThrows(IllegalArgumentException.class, () -> ui.parse("decode", "--target"));
+        // --source and --target lack values
         assertThrows(IllegalArgumentException.class, () -> ui.parse("decode", "--source", "--target"));
+        // --source needs many, --target needs one
         assertThrows(IllegalArgumentException.class, () -> ui.parse("decode", "--source", "a", "--target", "a", "b"));
 
+        // --password needs value
         assertThrows(IllegalArgumentException.class, () -> ui.parse("encode", "--source", "a", "--target", "b", "--password"));
+        // --compress is duplicated
         assertThrows(IllegalArgumentException.class, () -> ui.parse("encode", "--source", "a", "--target", "b", "--password", "pass", "--compress", "--compress"));
+        // --source, --compress, and --password are not allowed for analyze
         assertThrows(IllegalArgumentException.class, () -> ui.parse("analyze", "--source", "a", "--target", "b"));
         assertThrows(IllegalArgumentException.class, () -> ui.parse("analyze", "--target", "b", "--compress"));
         assertThrows(IllegalArgumentException.class, () -> ui.parse("analyze", "--target", "b", "--password", "pass"));
 
+        // valid arguments
         assertDoesNotThrow(() -> ui.parse("encode", "--source", "a", "--target", "b", "c"));
         assertDoesNotThrow(() -> ui.parse("decode", "--source", "a", "b", "--target", "c"));
         assertDoesNotThrow(() -> ui.parse("encode", "--source", "a", "--target", "b", "c", "--password", "pass"));
